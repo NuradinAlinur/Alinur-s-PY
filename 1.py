a3b7d2d14a6e75275c777c13bb59da57cff881e8
NuradinAlinur <nuradinalinur2@gmail.com>
2025-01-22 15:48:11 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,37 @@
+# number 1
+print("hello World")
+
+# syntax
+if 5 > 2:
+    print("Five is greater than two!")
+
+# this is a comment
+
+# Variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+# Data types
+x = 3
+print(type(x))
+
+
+# numbers
+x = 1
+y = 1.2
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Casting
+x = int(1)   # x will be 1
+y = int(2.8)  # y will be 2
+z = int("3")  # z will be 3
