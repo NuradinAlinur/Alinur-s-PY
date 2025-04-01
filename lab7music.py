46763e156c25f935f723e3b05d4081d3f8fa5f49
NuradinAlinur <nuradinalinur2@gmail.com>
2025-04-02 01:26:10 +0500

    lab7music

--- lab7music.py
+++ lab7music.py
@@ -1,12 +1,20 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((300, 300))
+screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
 
-# ПРАВИЛЬНЫЙ СПОСОБ УКАЗАНИЯ ПУТЕЙ
 background = pygame.image.load(
     r"C:\Users\nurad\OneDrive\Рабочий стол\KBTU\PY\lab7\AndreyGubin.jpg")
-background = pygame.transform.scale(background, (300, 300))
+
+
+def scale_image():
+    bg_width, bg_height = background.get_size()
+    screen_width, screen_height = screen.get_size()
+    scale_factor = min(screen_width / bg_width, screen_height / bg_height)
+    new_width = int(bg_width * scale_factor)
+    new_height = int(bg_height * scale_factor)
+    return pygame.transform.scale(background, (new_width, new_height)), new_width, new_height
+
 
 songs = [
     r"C:\Users\nurad\OneDrive\Рабочий стол\KBTU\Py\lab7\Andrejj_Gubin_-_Zabytyjj_tobojj_48331001.mp3",
@@ -15,10 +23,7 @@ songs = [
 ]
 
 current_song = 0
-
 pygame.mixer.init()
-
-# Запуск первой песни
 pygame.mixer.music.load(songs[current_song])
 pygame.mixer.music.play()
 
@@ -29,29 +34,34 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:  # Пауза и продолжение
+            if event.key == pygame.K_SPACE:
                 if music:
                     pygame.mixer.music.pause()
                     music = False
                 else:
                     pygame.mixer.music.unpause()
                     music = True
-
-            elif event.key == pygame.K_RIGHT:  # Следующая песня
+            elif event.key == pygame.K_RIGHT:
                 current_song += 1
                 if current_song >= len(songs):
                     current_song = 0
                 pygame.mixer.music.load(songs[current_song])
                 pygame.mixer.music.play()
-
-            elif event.key == pygame.K_LEFT:  # Предыдущая песня
+            elif event.key == pygame.K_LEFT:
                 current_song -= 1
                 if current_song < 0:
                     current_song = len(songs) - 1
                 pygame.mixer.music.load(songs[current_song])
                 pygame.mixer.music.play()
+            elif event.key == pygame.K_ESCAPE:
+                done = True
+        elif event.type == pygame.VIDEORESIZE:
+            screen = pygame.display.set_mode(
+                (event.w, event.h), pygame.RESIZABLE)
 
-    screen.blit(background, (0, 0))
+    scaled_bg, bg_width, bg_height = scale_image()
+    screen.fill((0, 0, 0))
+    screen.blit(scaled_bg, ((screen.get_width() - bg_width) //
+                2, (screen.get_height() - bg_height) // 2))
     pygame.display.flip()
